a9f62018359158c5ef1189213a5c3eaff83291f9
Edward D. Robertson <edwardd.robertson@Edwards-MacBook-Air.local>
2021-06-20 21:40:09 +0200

    third second

--- admin/includes/admin-navbar.php
+++ admin/includes/admin-navbar.php
@@ -1,8 +1,8 @@
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="index.html">Start Bootstrap</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button> 
-                    <a class="btn btn-info" href="login.html">Logout</a>
-            
+                    <a class="btn btn-info" href="logout.php">Logout</a>
+
             <!-- Navbar-->
            <!--  <ul class="navbar-nav ml-auto ml-md-0">
                 <li class="nav-item dropdown">
